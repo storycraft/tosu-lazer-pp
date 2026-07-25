7db42b15e0149cb6f1d2aaf35f6ab4b7b196d142
스도리 <storycraft@pancake.sh>
2026-07-25 12:39:48 +0900

    chore: cleanup code (#16)
    * remove confusing error throwing in FromLegacyGameMode

--- lib/native/Internal/Rulesets.cs
+++ lib/native/Internal/Rulesets.cs
@@ -1,4 +1,3 @@
-using System;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Catch;
 using osu.Game.Rulesets.Mania;
@@ -9,17 +8,22 @@ namespace binding.Internal;
 
 internal static class Rulesets
 {
-    public static readonly Ruleset osuRuleset = new OsuRuleset();
-    public static readonly Ruleset taikoRuleset = new TaikoRuleset();
-    public static readonly Ruleset catchRuleset = new CatchRuleset();
-    public static readonly Ruleset maniaRuleset = new ManiaRuleset();
+    public static readonly OsuRuleset osuRuleset = new();
+    public static readonly TaikoRuleset taikoRuleset = new();
+    public static readonly CatchRuleset catchRuleset = new();
+    public static readonly ManiaRuleset maniaRuleset = new();
 
+    /// <summary>
+    /// Returns a ruleset instance from a legacy online ruleset id.
+    /// </summary>
+    /// <param name="gameMode">Legacy online ruleset id</param>
+    /// <returns>An Ruleset instance implementing ILegacyRuleset</returns>
     public static Ruleset? FromLegacyGameMode(int gameMode) => gameMode switch
     {
         0 => osuRuleset,
         1 => taikoRuleset,
         2 => catchRuleset,
         3 => maniaRuleset,
-        _ => throw new ArgumentException($"Invalid ruleset for game mode: {gameMode}", nameof(gameMode)),
+        _ => null,
     };
 }
