a737aba01e6a60f27b57ad5364bd8739cb864633
storycraft <storycraft@pancake.sh>
2026-07-29 18:34:13 +0900

    fix: split original beatmap and playable beatmap

--- lib/native/Internal/DiffWorkingBeatmap.cs
+++ lib/native/Internal/DiffWorkingBeatmap.cs
@@ -8,8 +8,13 @@ namespace binding.Internal;
 
 internal sealed class DiffWorkingBeatmap : FlatWorkingBeatmap
 {
-    internal DiffWorkingBeatmap(IBeatmap beatmap) : base(beatmap) { }
+    private readonly IBeatmap playableBeatmap;
+
+    internal DiffWorkingBeatmap(IBeatmap beatmap, IBeatmap playableBeatmap) : base(beatmap)
+    {
+        this.playableBeatmap = playableBeatmap;
+    }
 
     // Bypass beatmap conversion logic since the beatmap is already playable state to given ruleset and mods.
-    public override IBeatmap GetPlayableBeatmap(IRulesetInfo ruleset, IReadOnlyList<Mod> mods, CancellationToken token) => Beatmap;
+    public override IBeatmap GetPlayableBeatmap(IRulesetInfo ruleset, IReadOnlyList<Mod> mods, CancellationToken token) => playableBeatmap;
 }
